e810b47c5324dd0f5a015dce067971b97f608954
flow2708 <dibirovamin10@gmail.com>
2026-01-07 22:09:28 +0300

    fix

--- src/main/java/com/example/demo/AuthenticatingLdapApplication.java
+++ src/main/java/com/example/demo/AuthenticatingLdapApplication.java
@@ -9,5 +9,4 @@ public class AuthenticatingLdapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticatingLdapApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,16 +20,21 @@ public class WebSecurityConfig {
                 )
                 .formLogin((form) -> form
                         .permitAll()
-                )
-                .logout((logout) -> logout
-                        .permitAll()
                 );
 
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordAttribute("userPassword");
     }
 }
